b46dd145a408d2f141c216ce36513099c4d589dc
Rizal Yogi Pratama <Rizal Yogi Pratama>
2016-10-20 18:32:10 +0700

    sql

---
+++ kirim_resep.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by Free CSS Templates
+http://www.freecsstemplates.org
+Released for free under a Creative Commons Attribution 2.5 License
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Black Fores</title>
+<meta name="Keywords" content="" />
+<meta name="Description" content="" />
+<link href="default.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="header">
+<img src="images/logo.png" alt="" width="130" height="110" class="img1" />
+<h1>Black Forest</h1>
+</div>
+<div id="content">
+
+	<div id="colOne">
+		<div id="menu1">
+			<ul>
+				<li id="menu-01"><a href="index.html">Home</a></li>
+				<li id="menu-02"><a href="sejarah.html">Sejarah</a></li>
+				<li id="menu-03"><a href="cara_membuat.html">Cara Membuat Makanan</a></li>
+				<li id="menu-04"><a href="galery.html">Galari Makanan</a></li>
+				<li id="menu-04"><a href="dampak.html">Efek Baik dan Buruk</a></li>
+				<li id="menu-04"><a href="fakta_kue.html">Fakta Kue</a></li>
+				<li id="menu-04"><a href="perbandingan kandungan.html">Perbandingan Kandungan</a></li>
+				<li id="menu-05"><a href="">Contact Us</a></li>
+			</ul>
+		</div>
+
+	</div>
+
+	<div id="colTwo">
+		<h2>Black Forres</h2>
+
+		<p><img src="https://cincinletseat.files.wordpress.com/2011/11/6293769064_e64db5bbae_o.jpg" alt="" width="130" height="110" class="img1" />
+		</p>
+		<p>
+			Kue Black Forest (Bahasa Jerman: Schwarzwälder Kirschtorte) adalah jenis kue khas Jerman yang paling dikenal di dunia.
+			 <br>Kue ini terbuat dari bolu coklat yang dilapisi krim segar, serutan coklat dan ceri yang direndam dalam <br>Kirschwasser, schnapps ceri jernih khas daerah Schwarzwälder ("Black Forest").
+
+		</p>
+		<p><img src="https://i.ytimg.com/vi/gCfhNGI4pRo/maxresdefault.jpg" alt="" width="130" height="110" class="img1" />
+		</p>
+		<p>
+			Black forest yang yummy saat di padukan dengan cherry yang segar dan mantap untuk disantap uhuy...
+
+		</p>
+		<p><br><br><br><br><br></p>
+		<p><img src="http://www.taste.com.au/images/recipes/tm/2013/09/cherry-ripple-black-forest-cake-26862_l.jpeg" alt="" width="130" height="110" class="img1" />
+		</p>
+		<p>
+			Black forest sangat lezat saat di padu dengan cokelat crispy serta krim coklat yang meleleh sangat bagus buat desert
+
+		</p>
+		<p><br><br><br><br><br></p>
+		<p><img src="https://i.ytimg.com/vi/8vJ6yUVnBWA/maxresdefault.jpg" alt="" width="130" height="110" class="img1" />
+		</p>
+		<p>
+			Black forest dengan butiran coklat sangat yummy
+
+		</p>
+
+	</div>
+	<div style="clear: both;">&nbsp;</div>
+</div>
+<div id="footer">
+<<<<<<< HEAD
+	<p>Copyright by Black Forrees - 2016</a></p>
+=======
+	<h3>Black Forrees by Team 2016</h3>
+>>>>>>> 73f657cbb1a18dd388e4fd64254cbab9fa977836
+</div>
+</body>
+</html>
+x
